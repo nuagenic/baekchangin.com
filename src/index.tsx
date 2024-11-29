4e3f10d72bd1acd3aa188caa54ba458327741476
nuagenic <nuagenic@gmail.com>
2024-11-29 23:40:56 +0900

    fix: Change css import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./styles/globals.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Root from "./routes/Root";
